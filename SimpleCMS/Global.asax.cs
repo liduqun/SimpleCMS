c56358b65829c327cc28fb98511a87b55ea3177b
mydell <liduqun@qq.com>
2018-03-04 00:12:15 +0800

    添加ExtJS应用程序前

--- SimpleCMS/Global.asax.cs
+++ SimpleCMS/Global.asax.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using SimpleCMS.Migrations;
+using SimpleCMS.Models;
+using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -7,12 +10,13 @@ using System.Web.Routing;
 
 namespace SimpleCMS
 {
-    public class MvcApplication : System.Web.HttpApplication
+  public class MvcApplication : System.Web.HttpApplication
+  {
+    protected void Application_Start()
     {
-        protected void Application_Start()
-        {
-            AreaRegistration.RegisterAllAreas();
-            RouteConfig.RegisterRoutes(RouteTable.Routes);
-        }
+      AreaRegistration.RegisterAllAreas();
+      RouteConfig.RegisterRoutes(RouteTable.Routes);
+      Database.SetInitializer(new MigrateDatabaseToLatestVersion<ApplicationDbContext, Configuration>());
     }
+  }
 }
---
+++ SimpleCMS/Models/ApplicationDbContext.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+using NLog;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace SimpleCMS.Models
+{
+  public class ApplicationDbContext : IdentityDbContext<ApplicationUser, ApplicationRole, int, ApplicationUserLogin, ApplicationUserRole, ApplicationUserClaim>
+  {
+    public ApplicationDbContext() : base("DefaultConnection")
+    {
+      Database.Log = Logger.Info;
+    }
+    private static readonly Logger Logger = LogManager.GetCurrentClassLogger();
+    public static ApplicationDbContext Create()
+    {
+      return new ApplicationDbContext();
+    }
+    public DbSet<Category> Categories { get; set; }
+    public DbSet<Content> Contents { get; set; }
+    public DbSet<Media> Mediae { get; set; }
+    public DbSet<Tag> Tags { get; set; }
+  }
+}
